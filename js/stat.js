672293c6046a6d2e4354ebdc245ce3a09ac8976a
Garmahis666 <garmahis@garmahis.ru>
2018-09-12 23:59:25 +0300

    Update stat.js

--- js/stat.js
+++ js/stat.js
@@ -1,16 +1,36 @@
 'use strict';
+var windowParams = {
+  X: 100,
+  Y: 10,
+  WIDTH: 420,
+  HEIGHT: 270,
+  COLOR: '#fff',
+  SHADOW_OFFSET: 10,
+  SHADOW_COLOR: 'rgba(0, 0, 0, 0.7)'
+};
 
-var WINDOW_WIDTH = 420;
-var WINDOW_HEIGHT = 270;
-var WINDOW_X = 100;
-var WINDOW_Y = 10;
-var SHADOW_OFFSET = 10;
-var FONT = '16px PT Mono';
-var HEADER_GAP = 80;
-var CHART_WIDTH = 40;
-var CHART_HEIGHT = 150;
-var BORDER_GAP = 40;
-var GAP = 10;
+var headerParams = {
+  X_GAP: 80,
+  Y_GAP: 20,
+  BORDER: 17,
+  FONT_COLOR: '#000'
+};
+
+var barParams = {
+  WIDTH: 40,
+  HEIGHT: 150,
+  X_GAP: 40,
+  Y_GAP: 20,
+  BORDER: 20
+};
+
+var fontParams = {
+  SIZE: 16,
+  FONT: 'PT Mono',
+  getFont: function () {
+    return this.SIZE + 'px ' + this.FONT;
+  }
+};
 
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
@@ -27,34 +47,36 @@ var renderRect = function (ctx, x, y, width, height, windowStyle) {
   ctx.fillRect(x, y, width, height);
 };
 
-var renderText = function (ctx, text, x, y) {
-  ctx.fillStyle = '#000';
-  ctx.font = FONT;
+var renderText = function (ctx, text, x, y, style) {
+  ctx.fillStyle = style;
+  ctx.font = fontParams.getFont();
   ctx.fillText(text, x, y);
 };
 
 var renderBar = function (ctx, name, time, maxTime, x, y) {
-  renderText(ctx, name, x, y);
-  renderText(ctx, (time / 1000).toFixed(1), x, y - (CHART_HEIGHT + GAP * 3));
-  var barHeight = Math.round(CHART_HEIGHT * time / maxTime);
-  var style = 'rgba(255, 0, 0, 1)';
-  if (name !== 'Вы') {
-    style = 'rgba(0, 0, ' + Math.floor(Math.random() * 256) + ', 1)';
-  }
-  renderRect(ctx, x, y - GAP * 2, CHART_WIDTH, 0 - barHeight, style);
+  var barHeight = Math.round(barParams.HEIGHT * time / maxTime);
+  var style = (name === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, ' + Math.floor(Math.random() * 255) + 1 + ', 1)';
+  renderText(ctx, name, x, y, style);
+  renderText(ctx, (time / 1000).toFixed(3), x, (y - barHeight) - barParams.BORDER * 1.5, style);
+  renderRect(ctx, x, y - barParams.BORDER, barParams.WIDTH, 0 - barHeight, style);
 };
 
 window.renderStatistics = function (ctx, names, times) {
   // Рисуем окно
-  renderRect(ctx, WINDOW_X + SHADOW_OFFSET, WINDOW_Y + SHADOW_OFFSET, WINDOW_WIDTH, WINDOW_HEIGHT, 'rgba(0, 0, 0, 0.7)');
-  renderRect(ctx, WINDOW_X, WINDOW_Y, WINDOW_WIDTH, WINDOW_HEIGHT, '#fff');
+  renderRect(ctx, windowParams.X + windowParams.SHADOW_OFFSET, windowParams.Y + windowParams.SHADOW_OFFSET,
+      windowParams.WIDTH, windowParams.HEIGHT, windowParams.SHADOW_COLOR);
+  renderRect(ctx, windowParams.X, windowParams.Y, windowParams.WIDTH, windowParams.HEIGHT, windowParams.COLOR);
   // Пишем текст
-  renderText(ctx, 'Ура вы победили!', WINDOW_X + HEADER_GAP, WINDOW_Y + GAP * 2);
-  renderText(ctx, 'Список результатов:', WINDOW_X + HEADER_GAP, WINDOW_Y + GAP * 4);
+  renderText(ctx, 'Ура вы победили!', windowParams.X + headerParams.X_GAP, windowParams.Y + headerParams.Y_GAP,
+      headerParams.FONT_COLOR);
+  renderText(ctx, 'Список результатов:', windowParams.X + headerParams.X_GAP,
+      windowParams.Y + headerParams.Y_GAP + headerParams.BORDER, headerParams.FONT_COLOR);
   // Рисуем бары
   var maxResult = getMaxElement(times);
   for (var i = 0; i < names.length; i++) {
-    renderBar(ctx, names[i], times[i], maxResult, WINDOW_X + BORDER_GAP + ((WINDOW_WIDTH - GAP * 2) / names.length) * i, WINDOW_Y + WINDOW_HEIGHT - GAP);
+    renderBar(ctx, names[i], times[i], maxResult,
+        windowParams.X + barParams.X_GAP + ((windowParams.WIDTH - barParams.BORDER) / names.length) * i,
+        windowParams.Y + windowParams.HEIGHT - barParams.Y_GAP);
   }
 };
 
